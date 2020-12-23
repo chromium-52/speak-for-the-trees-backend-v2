0cbe450e5f13980896e631cc9ed891cad0e73957
unknown <florisdobber@gmail.com>
2020-12-23 21:47:47 +0100

    Fixed privilege level field

--- persist/src/main/resources/db/migration/V1__Initial_Import.sql
+++ persist/src/main/resources/db/migration/V1__Initial_Import.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users
     first_name      VARCHAR(36) NOT NULL,
     last_name       VARCHAR(36) NOT NULL,
     username        VARCHAR(36) NOT NULL,
-    privilege_level VARCHAR(10) NOT NULL DEFAULT '0',
+    privilege_level VARCHAR(20) NOT NULL DEFAULT 'Member',
     password_hash   BYTEA       NOT NULL
 );
 
