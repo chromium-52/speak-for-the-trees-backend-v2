de9f3408d00ecb047ae83d26038dfd892ae5dcf6
unknown <florisdobber@gmail.com>
2020-12-30 21:30:39 +0100

    Forgot to register change privilege level route

--- api/src/main/java/com/codeforcommunity/rest/subrouter/ProtectedUserRouter.java
+++ api/src/main/java/com/codeforcommunity/rest/subrouter/ProtectedUserRouter.java
@@ -32,6 +32,7 @@ public class ProtectedUserRouter implements IRouter {
     registerChangePassword(router);
     registerGetUserData(router);
     registerChangeEmail(router);
+    registerChangePrivilegeLevel(router);
 
     return router;
   }
