0eeb9cdec8242b3c8e81eff40898217aaf1ec40f
unknown <florisdobber@gmail.com>
2021-05-19 15:45:49 +0200

    Fixed hopefully

--- service/src/main/java/com/codeforcommunity/processor/SiteProcessorImpl.java
+++ service/src/main/java/com/codeforcommunity/processor/SiteProcessorImpl.java
@@ -58,19 +58,20 @@ public class SiteProcessorImpl implements ISiteProcessor {
           }
 
           // Finds if the site is adopted, and if it is returns the username of the adopter
-//          String adopter;
-//          AdoptedSitesRecord adoptedSitesRecord =
-//              db.selectFrom(ADOPTED_SITES).where(ADOPTED_SITES.SITE_ID.eq(siteId)).fetchOne();
-//          if (adoptedSitesRecord == null) {
-//            adopter = null;
-//          } else {
-//            adopter =
-//                db.select(USERS.USERNAME)
-//                    .from(USERS)
-//                    .where(USERS.ID.eq(adoptedSitesRecord.getUserId()))
-//                    .fetchOne()
-//                    .value1();
-//          }
+          //          String adopter;
+          //          AdoptedSitesRecord adoptedSitesRecord =
+          //
+          // db.selectFrom(ADOPTED_SITES).where(ADOPTED_SITES.SITE_ID.eq(siteId)).fetchOne();
+          //          if (adoptedSitesRecord == null) {
+          //            adopter = null;
+          //          } else {
+          //            adopter =
+          //                db.select(USERS.USERNAME)
+          //                    .from(USERS)
+          //                    .where(USERS.ID.eq(adoptedSitesRecord.getUserId()))
+          //                    .fetchOne()
+          //                    .value1();
+          //          }
 
           SiteEntry siteEntry =
               new SiteEntry(
@@ -115,7 +116,7 @@ public class SiteProcessorImpl implements ISiteProcessor {
                   record.getStump(),
                   record.getTreeNotes(),
                   record.getSiteNotes(),
-                  null); //TODO: Fix
+                  null); // TODO: Fix
 
           siteEntries.add(siteEntry);
         });
