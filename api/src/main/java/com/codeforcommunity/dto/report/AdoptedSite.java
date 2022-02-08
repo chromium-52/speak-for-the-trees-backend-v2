eb29789f8922980648f968486a96d2289ec4974c
chromium-52 <jung.du@northeastern.edu>
2022-02-08 18:19:40 -0500

    Fix spacing from 4 spaces to 2 spaces

--- api/src/main/java/com/codeforcommunity/dto/report/AdoptedSite.java
+++ api/src/main/java/com/codeforcommunity/dto/report/AdoptedSite.java
@@ -5,59 +5,59 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.sql.Timestamp;
 
 public class AdoptedSite {
-    private final int siteId;
-    private final String address;
-    private final String name;
-    private final String email;
+  private final int siteId;
+  private final String address;
+  private final String name;
+  private final String email;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
-    private final Timestamp dateAdopted;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+  private final Timestamp dateAdopted;
 
-    private final int activityCount;
-    private final String neighborhood;
+  private final int activityCount;
+  private final String neighborhood;
 
-    public AdoptedSite(
-            int siteId,
-            String address,
-            String name,
-            String email,
-            Timestamp dateAdopted,
-            int activityCount,
-            String neighborhood) {
-        this.siteId = siteId;
-        this.address = address;
-        this.name = name;
-        this.email = email;
-        this.dateAdopted = dateAdopted;
-        this.activityCount = activityCount;
-        this.neighborhood = neighborhood;
-    }
+  public AdoptedSite(
+      int siteId,
+      String address,
+      String name,
+      String email,
+      Timestamp dateAdopted,
+      int activityCount,
+      String neighborhood) {
+    this.siteId = siteId;
+    this.address = address;
+    this.name = name;
+    this.email = email;
+    this.dateAdopted = dateAdopted;
+    this.activityCount = activityCount;
+    this.neighborhood = neighborhood;
+  }
 
-    public int getSiteId() {
-        return siteId;
-    }
+  public int getSiteId() {
+    return siteId;
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public String getAddress() {
+    return address;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public Timestamp getDateAdopted() {
-        return dateAdopted;
-    }
+  public Timestamp getDateAdopted() {
+    return dateAdopted;
+  }
 
-    public int getActivityCount() {
-        return activityCount;
-    }
+  public int getActivityCount() {
+    return activityCount;
+  }
 
-    public String getNeighborhood() {
-        return neighborhood;
-    }
+  public String getNeighborhood() {
+    return neighborhood;
+  }
 }
--- api/src/main/java/com/codeforcommunity/dto/report/GetReportCSVRequest.java
+++ api/src/main/java/com/codeforcommunity/dto/report/GetReportCSVRequest.java
@@ -6,31 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GetReportCSVRequest extends ApiDto {
-    private Long previousDays;
+  private Long previousDays;
 
-    public GetReportCSVRequest(Long previousDays) {
-        this.previousDays = previousDays;
+  public GetReportCSVRequest(Long previousDays) {
+    this.previousDays = previousDays;
+  }
+
+  private GetReportCSVRequest() {}
+
+  public Long getPreviousDays() {
+    return previousDays;
+  }
+
+  public void setPreviousDays(Long previousDays) {
+    this.previousDays = previousDays;
+  }
+
+  @Override
+  public List<String> validateFields(String fieldPrefix) throws HandledException {
+    String fieldName = fieldPrefix + "report_csv_request";
+    List<String> fields = new ArrayList<>();
+
+    if (previousDays == null) {
+      fields.add(fieldName + "previousDays");
     }
 
-    private GetReportCSVRequest() {}
-
-    public Long getPreviousDays() {
-        return previousDays;
-    }
-
-    public void setPreviousDays(Long previousDays) {
-        this.previousDays = previousDays;
-    }
-
-    @Override
-    public List<String> validateFields(String fieldPrefix) throws HandledException {
-        String fieldName = fieldPrefix + "report_csv_request";
-        List<String> fields = new ArrayList<>();
-
-        if (previousDays == null) {
-            fields.add(fieldName + "previousDays");
-        }
-
-        return fields;
-    }
+    return fields;
+  }
 }
