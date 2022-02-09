6c9029dbca1b677a7721343f7cd61c58f524e549
Joe Hirsch <joseph.hirsch.dev@gmail.com>
2022-02-09 16:29:34 -0500

    Fixed import statements

--- service/src/main/java/com/codeforcommunity/processor/ProtectedSiteProcessorImpl.java
+++ service/src/main/java/com/codeforcommunity/processor/ProtectedSiteProcessorImpl.java
@@ -1,6 +1,12 @@
 package com.codeforcommunity.processor;
 
-import static org.jooq.generated.Tables.*;
+import static org.jooq.generated.Tables.ADOPTED_SITES;
+import static org.jooq.generated.Tables.BLOCKS;
+import static org.jooq.generated.Tables.NEIGHBORHOODS;
+import static org.jooq.generated.Tables.SITES;
+import static org.jooq.generated.Tables.USERS;
+import static org.jooq.generated.Tables.SITE_ENTRIES;
+import static org.jooq.generated.Tables.STEWARDSHIP;
 import static org.jooq.impl.DSL.max;
 
 import com.codeforcommunity.api.IProtectedSiteProcessor;
@@ -19,9 +25,11 @@ import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.List;
 import org.jooq.DSLContext;
-import org.jooq.Record1;
-import org.jooq.Result;
-import org.jooq.generated.tables.records.*;
+import org.jooq.generated.tables.records.AdoptedSitesRecord;
+import org.jooq.generated.tables.records.SiteEntriesRecord;
+import org.jooq.generated.tables.records.SitesRecord;
+import org.jooq.generated.tables.records.UsersRecord;
+import org.jooq.generated.tables.records.StewardshipRecord;
 
 public class ProtectedSiteProcessorImpl implements IProtectedSiteProcessor {
 
