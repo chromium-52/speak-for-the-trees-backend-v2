bceb4f052a6b9acd7f1bb57dabea83eb0614ce96
Joe Hirsch <joseph.hirsch.dev@gmail.com>
2022-02-16 15:33:58 -0500

    Created migration script for adding parent account table

---
+++ persist/src/main/resources/db/migration/V3_11__AddParentAccount.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS parent_accounts (
+    parent_id     INT     NOT NULL,
+    child_id      INT     NOT NULL,
+
+    CONSTRAINT parent_accounts_parent_id_fk FOREIGN KEY (parent_id) REFERENCES users (id),
+    CONSTRAINT parent_accounts_child_id_fk FOREIGN KEY (child_id) REFERENCES users (id)
+);
